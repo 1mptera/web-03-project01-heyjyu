084db14260702916347db1320be405e182799733
heyjyu <jiwoosmile@yonsei.ac.kr>
2022-09-21 09:08:00 +0900

    Add journal repository
    Make CRUD for journal repository.

---
+++ src/main/java/repositories/JournalRepository.java
@@ -0,0 +1,52 @@
+package repositories;
+
+import models.Journal;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public class JournalRepository {
+    private static JournalRepository instance = new JournalRepository();
+
+    private List<Journal> journals;
+
+    private JournalRepository() {
+        journals = new ArrayList<>(loadJournals());
+    }
+
+    private List<Journal> loadJournals() {
+        return List.of(); //TODO get from file
+    }
+
+    public static JournalRepository getInstance() {
+        return instance;
+    }
+
+    public void add(Journal journal) {
+        journals.add(journal);
+    }
+
+    public Journal getById(UUID id) {
+        Optional<Journal> element = journals.stream()
+                .filter(journal -> journal.getId().equals(id))
+                .findFirst();
+
+        return element.get();
+    }
+
+    public List<Journal> getJournals() {
+        return journals;
+    }
+
+    public void removeById(UUID id) {
+        journals.remove(getById(id));
+    }
+
+    public void updateById(UUID id, String content) {
+        Journal journal = getById(id);
+
+        journal.modify(content);
+    }
+}
